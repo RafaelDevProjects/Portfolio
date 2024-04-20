3ec96ac5571a6baaead8fc0306c7e73ed20608db
Rafael De Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2024-04-20 14:27:47 -0300

    deixando o scroll com comportamento smooth

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const NotFound = () => {
   return (
     <div>
-      <h1>404 - Página não encontrada</h1>
-      <p>A página que você está procurando não existe.</p>
+      <h1>404 - PageNotFound</h1>
+      <p>The page you are looking for does not exist.</p>
     </div>
   );
 };
